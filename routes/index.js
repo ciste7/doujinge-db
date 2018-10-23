1b23e04d38d0aa186512f8ff06681282cfa9f8e2
ciste7 <ciste7@protonmail.com>
2018-10-23 23:39:59 +1300

    Add background and site logo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+// *                                                        * General Init * //
+const website_name = 'Doujinge DB';
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+// *                                                             * Routing * //
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: website_name });
 });
+
 
 module.exports = router;
